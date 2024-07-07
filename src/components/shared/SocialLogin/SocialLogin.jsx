8df0834ae41003f3d99aab0bf192c6a14602e072
towhid046 <towhidmorol46@gmail.com>
2024-07-07 09:02:43 +0600

    ...

--- src/components/shared/SocialLogin/SocialLogin.jsx
+++ src/components/shared/SocialLogin/SocialLogin.jsx
@@ -1,15 +1,15 @@
-import { FaGoogle, FaGithub} from "react-icons/fa";
+import { FaGoogle, FaGithub } from "react-icons/fa";
 const SocialLogin = () => {
   return (
-         <div className="flex items-center justify-center  gap-3">
-            <button className="btn h-12 w-12 rounded-full">
-              <FaGithub className="text-blue-500 text-2xl" />
-            </button>
-            <button className="btn h-12 w-12 rounded-full">
-              <FaGoogle className=" text-2xl" />
-            </button>
-          </div>
-  )
-}
+    <div className="flex items-center justify-center  gap-3">
+      <button className="btn h-12 w-12 rounded-full">
+        <FaGithub className="text-blue-500 text-2xl" />
+      </button>
+      <button className="btn h-12 w-12 rounded-full">
+        <FaGoogle className=" text-2xl" />
+      </button>
+    </div>
+  );
+};
 
-export default SocialLogin
+export default SocialLogin;
